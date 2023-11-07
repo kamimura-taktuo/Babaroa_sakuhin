46eb1a54116599b6e633e4a8f907aaedce2ec707
kamimura-takuto <22s015@ca.harada-gakuen.ac.jp>
2023-11-07 12:06:30 +0900

    11/07_ilnk

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,9 +16,24 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css' },
+      { rel: 'stylesheet' , href: 'https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/4-1-4/css/4-1-4.css' },
+      { rel: 'stylesheet' , href: 'https://fonts.googleapis.com/earlyaccess/hannari.css' },
+      { rel: 'stylesheet' , href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css' },
+      { rel: 'stylesheet' , href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css' },
+      { rel: 'preconnect' , href: 'https://fonts.googleapis.com'},
+      { rel: 'preconnect' , href: 'https://fonts.gstatic.com'},
+      { rel: 'stylesheet' , href: 'https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@1,300&display=swap'},
+    ],
+    script: [
       {src:'~assets/js/main.js'},
-      {src:'~assets/js/main.js'}
-    ]
+      {src:'~assets/js/main.js'},
+      {src:'https://code.jquery.com/jquery-3.4.1.min.js'},
+      {src:'https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/4-1-4/js/4-1-4.js'},
+      {src:'https://code.jquery.com/jquery-1.11.0.min.js'},
+      {src:'https://code.jquery.com/jquery-migrate-1.2.1.min.js'},
+      {src:'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js'}
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
