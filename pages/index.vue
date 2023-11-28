af5c8724107b335fa2fc95efae1b71228f7cd633
harada-nagamine <133723000+harada-nagamine@users.noreply.github.com>
2023-11-28 13:45:58 +0900

    11/28_nagamine

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
-
-  
   <div>
-    <TheHeader />
-
     <!-- <section class="container">
     <div>
       <div class="logoWrapper">
@@ -13,6 +9,7 @@
     </div>
     </section> -->
     <!-- <slick ref="slick" :options="option">
+
       <div>
         aaa
       </div>
@@ -26,16 +23,55 @@
         ddd
       </div>
     </slick> -->
-    <div id="splash" >
-          <div id="splash_logo">
-            <img src="../assets/img/logo.png" alt="ロゴ" class="fadeUp" />
-          </div>
+
+    <!--ロード画面のロゴ-->
+        <div id="splash">
+        <div id="splash_logo">
+              <img src="../assets/img/logo.png" alt="ロゴ" class="fadeUp">
         </div>
+        </div>
+
+
+    <!--ヘッダー-->
+        <header class="header">
+          <div class="header-inner inner">
+            <div class="inner float">
+    <!--ヘッダー内のロゴ-->
+              <div class="logo">
+                <h1 class="header-title">
+                  <a href="http://localhost:3000/">
+                    <img
+                      src="../assets/img/logo.png"
+                      alt="rogo"
+                      width="65"
+                      height="65"
+                    />
+                  </a>
+                </h1>
+              </div>
+            </div>
+              <!--検索機能-->
+              <form id="form4" action="http://localhost:3000/" method="get">
+              <input id="sbox4" name="s" type="text" v-model="searchText" placeholder="フリーワードを入力" />
+              <button id="sbtn4" type="button" @click="searchRecipe">検索</button>
+              </form>
+              <!--メニュー一覧-->
+              <nav class="header-nav">
+                <ul class="header-nav-list">
+                  <li class="header-nav-item"><a class="header-nav-item-link" href="#">カテゴリー</a></li>
+                  <li class="header-nav-item"><a class="header-nav-item-link" href="#">オヌヌメ</a></li>
+                  <li class="header-nav-item"><a class="header-nav-item-link" href="#">ニュース</a></li>
+                  <li class="header-nav-item"><a class="header-nav-item-link" href="#">ブログ</a></li>
+                  <li class="header-nav-item"><a class="header-nav-item-link" href="#">問い合わせ</a></li>
+                </ul>
+              </nav>
+            </div>
+          </header>
  
         <div class="relative">
-          
           <!-- <div class="your-class"> -->
-          <slick ref="slick" :options="option">
+          <!--動画のslick再生-->
+          <slick  :options="option">
             <div>
               <video id="video" class="video1"  autoplay loop muted playsinline>
                 <source
@@ -65,13 +101,14 @@
               </video>
             </div>
           </slick>
+        </div>
         
- 
+        <div>
           <img class="absolute" src="../assets/img/logo.png" />
           <div class="inner float">
             <div class="logo">
               <h1>
-                <a href="index.vue">
+                <a href="http://localhost:3000/">
                   <img
                     src="../assets/img/logo.png"
                     alt="rogo"
@@ -83,16 +120,16 @@
             </div>
           </div>
         </div>
-
+ 
     <div style="text-align: center; margin: auto;" >
         <p class="title">~One step closer to fashion~</p>
         <p class=p1>大人なスイーツレシピ</p>
     </div>
-
+ 
     <!--おすすめスイーツ-->
         <p class ="p4">おすすめスイーツ</p>
-          <div class="slider-for">
-            <div>
+          <slick :options="option2" class="slider-for" ref="sliderFor">
+              <div>
                   <div class="slide-item__wrapper2" style="display: flex;">
                       <div class="slide-item__img" style="flex: 0 0 50%;"><img style="width: 75%;"  class="png_1" src="../assets/img/どら焼き画像.png"></div>
                         <div class="slide-item__text">
@@ -156,10 +193,18 @@
                         </div>
                   </div>
                 </div>
-            </div>
-          
-
-    <div></div>
+          </slick>
+        
+      <!-- <slick :options="option3"> -->
+        <slick :options="option3" class="slider-nav" ref="sliderNav">
+          <div><img class="png_2" src="@/assets/img/どら焼き画像.png"><p class="p6">どら焼き</p></div>
+          <div><img class="png_2" src="@/assets/img/おはぎ画像.png"><p class="p6">おはぎ</p></div>
+          <div><img class="png_2" src="@/assets/img/わらび餅.png"><p class="p6">わらび餅</p></div>
+          <div><img class="png_2" src="@/assets/img/アップルパイ.png"><p class="p6">アップルパイ</p></div>
+          <div><img class="png_2" src="@/assets/img/カッサータ.png"><p class="p6">カッサータ</p></div>
+          <div><img class="png_2" src="@/assets/img/チーズタルト.png"><p class="p6">チーズタルト</p></div>
+          <div><img class="png_2" src="@/assets/img/抹茶カヌレ.png"><p class="p6">抹茶カヌレ</p></div>
+        </slick>
     <ul>
       <li v-for="content in contents" :key="content.id">
         <nuxt-link :to="`/${content.id}`">
@@ -168,7 +213,19 @@
       </li>
     </ul>
 
-    <TheFooter />
+
+    <!-- footer -->
+         <footer>
+          <ul class="footer-menu">
+           <li>home</li>
+           <li>about</li>
+           <li>service</li>
+           <li>Contact Us</li>
+          </ul>
+          <p>© All rights reserved by dmmwebcampmedia.</p>
+        </footer>
+
+    
   </div>
   
 </template>
@@ -183,6 +240,35 @@ export default {
     slick,
   },
   data() {
+    return{
+      option: {
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        infinite: false,
+        example: "test",
+      },
+      searchText: '',
+      option2:{
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        // asNavFor: this.$refs.sliderNav
+        asNavFor: '.slider-nav',
+      },
+      option3:{
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // asNavFor: this.$refs.sliderFor,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      }
+    }
   },
   async asyncData() {
     const { data } = await axios.get(
@@ -194,11 +280,6 @@ export default {
       }
     );
     return data;
-  },
-  data() {
-    return {
-      example: "test",
-    };
   },
   head() {
       return {
@@ -213,9 +294,7 @@ export default {
         ]
       }
   },
-  /*components:{
-    Logo
-  },*/
+
   methods:{
     execHideElement(selector){
       if(process.browser){
@@ -223,7 +302,11 @@ export default {
 
         main.hideElement(selector)
       }
-    }
+    },
+    searchRecipe() {
+      console.log(this.searchText)
+      // console.log(this.$params)
+    },
   },
   mounted() {
     // このページで動くスクリプト
