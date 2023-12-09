5662525a069f14f244665b07cae9b3958c5a7eef
harada-nagamine <133723000+harada-nagamine@users.noreply.github.com>
2023-12-09 11:54:13 +0900

    Update index.vue

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,19 +1,23 @@
 <template>
-  <main class="main">
-    <h1 class="title">{{ category_name }}</h1>
-    <!-- <p class="publishedAt">{{ publishedAt }}</p> -->
-    <div class="post" v-html="category_select.main"></div>
-  </main>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+      <img :src=content.eyecatch.url width="40%"><br>
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script>
 import axios from 'axios'
-
 export default {
-  async asyncData({ params }) {
+  async asyncData() {
     const { data } = await axios.get(
-      `https://test1024.microcms.io/api/v1/categories/${params.slug}`,
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://test1024.microcms.io/api/v1/menu',
       {
+        // your-api-key部分は自分のapi-keyに置き換えてください
         headers: { 'X-MICROCMS-API-KEY': 'Hwlkh7zsv3NQTyceA44qLqRecQ1ocae1NRGi' }
       }
     )
