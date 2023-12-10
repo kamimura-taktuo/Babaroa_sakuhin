06c05102afc55c93d119bdebe45ad57c3c00f114
harada-nagamine <133723000+harada-nagamine@users.noreply.github.com>
2023-12-10 23:09:40 +0900

    12/10

--- pages/_slug/recipe/index.vue
+++ pages/_slug/recipe/index.vue
@@ -1,9 +1,8 @@
 <template>
   <main class="main">
-    <h1 class="title">{{ category_name }}</h1>
-    <!-- <p class="publishedAt">{{ publishedAt }}</p> -->
-    <p class="category">{{ category && category.category_select }}</p>
-    <div class="post" v-html="body"></div>
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="main"></div>
   </main>
 </template>
 
@@ -13,7 +12,7 @@ import axios from 'axios'
 export default {
   async asyncData({ params }) {
     const { data } = await axios.get(
-      `https://test1024.microcms.io/api/v1/categories/${params.slug}`,
+      `https://test1024.microcms.io/api/v1/menu/${params.recipe}`,
       {
         headers: { 'X-MICROCMS-API-KEY': 'Hwlkh7zsv3NQTyceA44qLqRecQ1ocae1NRGi' }
       }
