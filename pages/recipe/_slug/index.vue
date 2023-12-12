0166f840fcb671d4ecd2bdaaf5ce01bd0e88323c
harada-nagamine <133723000+harada-nagamine@users.noreply.github.com>
2023-12-12 14:58:43 +0900

    12/12nagamine

--- pages/recipe/_slug/index.vue
+++ pages/recipe/_slug/index.vue
@@ -1,8 +1,28 @@
 <template>
   <main class="main">
     <h1 class="title">{{ title }}</h1>
-    <p class="publishedAt">{{ publishedAt }}</p>
-    <div class="post" v-html="body"></div>
+    <!--スイーツの画像とカロリーの並び-->
+    <div class="calorie">
+      <div  style="display: flex;">
+        <div class="img_setting" style="flex: 0 0 50%;"><img class="recipe_img" :src="calorie.img.url"></div>
+          <div class="text_setting" style="flex: 0 0 50%;">
+            <div class="calorie_text" v-html="calorie.main"></div>
+          </div>
+      </div>
+    </div>
+
+    <div class="material" v-html="main"></div>
+
+    <ul>
+      <li v-for="contents in tags" :key="contents.id">
+        <nuxt-link :to="`../tag/${contents.id}`">
+          {{ contents.tags_name}}
+        </nuxt-link>
+      </li>
+    </ul>
+
+
+
   </main>
 </template>
 
