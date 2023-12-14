9fe8691afa49279266c7eedb996db2a49ee625a9
kamimura-taktuo <22s015@ca.harada-gakuen.ac.jp>
2023-12-14 11:31:42 +0900

    12/14

---
+++ pages/tag/_slug/index.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <!--左上のロゴの表示-->
+  <div class="logo">
+    <h1 class="header-title">
+      <a href="http://localhost:3000/">
+        <img
+          src="@/assets/img/logo.png"
+          alt="rogo"
+          width="65"
+          height="65"
+        />
+      </a>
+    </h1>
+  </div>
+<ul class="recipe">
+  <li class="recipe_hairetu" v-for="content in contents" :key="content.id">
+    <nuxt-link :to="`../recipe/${content.id}`">
+    <img :src=content.eyecatch.url width="40%"><br>
+      {{ content.tags_name}}
+      {{content.title}}
+    </nuxt-link>
+  </li>
+</ul>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData({params}) {
+    console.log(params.slug)
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      `https://test1024.microcms.io/api/v1/menu?filters=tags[contains]${params.slug}`,
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'Hwlkh7zsv3NQTyceA44qLqRecQ1ocae1NRGi' }
+      }
+    )
+    return data
+  }
+  
+}
+</script>
